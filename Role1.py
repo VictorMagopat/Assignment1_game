3f6e94c89bd83724aff5d614290a92f4f49e531f
VictorMagopat <magopat.victor@gmail.com>
2023-10-01 08:13:09 -0700

    Added Attributes
    Added Attributes to The Spy and to The Thief.

--- Role1.py
+++ Role1.py
@@ -1,6 +1,37 @@
-#This is the Role1.py file
+#This is the Role1.py file.
+# This file implements the data and logic associated with "The Spy".
 
+# the character in this game
+IamTheSpy = "I am The Spy!"
 
+#The Spy attributes
+TheSpyHealth = 5
+TheSpyDexterity = 0
+TheSpyIntelligence = 2
+TheSpyCharisma	= 2
+TheSpyCombat = -1
+
+# returns the function of this character
 def WhatIsYourCharacter():
-    print("This is Role 1")
+    return IamTheSpy
+
+# returns the Health attribute
+def WhatIsYourHealth():
+    return TheSpyHealth
+
+# returns the Dexterity attribute
+def WhatIsYourDexterity():
+    return TheSpyDexterity
+
+# returns the Intelligence attribute
+def WhatIsYourIntelligence():
+    return TheSpyIntelligence
+
+# returns the Charisma attribute
+def WhatIsYourCharisma():
+    return TheSpyCharisma
+
+# returns the Combat attribute
+def WhatIsYourCombat():
+    return TheSpyCombat
 
--- Role2.py
+++ Role2.py
@@ -1,5 +1,37 @@
 #This is the Role2.py file
+# This file implements the data and logic associated with "The Thief".
 
 
+# the character in this game
+IamTheThief = "I am The Thief!"
+
+#The Thief attributes
+TheThiefHealth = 3
+TheThiefDexterity = 2
+TheThiefIntelligence = 2
+TheThiefCharisma	= 0
+TheThiefCombat = 2
+
+# returns the function of this character
 def WhatIsYourCharacter():
-    print("This is Role 2")
+    return IamTheThief
+
+# returns the Health attribute
+def WhatIsYourHealth():
+    return TheThiefHealth
+
+# returns the Dexterity attribute
+def WhatIsYourDexterity():
+    return TheThiefDexterity
+
+# returns the Intelligence attribute
+def WhatIsYourIntelligence():
+    return TheThiefIntelligence
+
+# returns the Charisma attribute
+def WhatIsYourCharisma():
+    return TheThiefCharisma
+
+# returns the Combat attribute
+def WhatIsYourCombat():
+    return TheThiefCombat
